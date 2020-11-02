82062fefe2554fd5c03d51f1713715166809d0af
Jai Raj <44637563+Raj-Jai@users.noreply.github.com>
2020-11-02 10:33:51 +0530

    updated time

--- scriptLockSchool.js
+++ scriptLockSchool.js
@@ -14,13 +14,13 @@ function random() {
     if (n == 2) {
         document.getElementById("tuesday").classList.add("current");
 
-        if (hm>= 390 && hm<= 450) {
+        if (hm>= 510 && hm<= 570) {
             document.getElementById("t1").classList.add("ctime");
-        } else if (hm >= 485 && hm <= 545) {
+        } else if (hm >= 595 && hm <= 655) {
             document.getElementById("t2").classList.add("ctime");
-        } else if (hm>= 565 && hm<= 625) {
+        } else if (hm>= 675 && hm<= 735) {
             document.getElementById("t3").classList.add("ctime");
-        } else if (hm>= 725 && hm<= 785) {
+        } else if (hm>= 755 && hm<= 815) {
             document.getElementById("t4").classList.add("ctime");
         } else {
             document.getElementById("sunday").innerHTML = ("It's break Time");
@@ -30,13 +30,13 @@ function random() {
 
     } else if (n == 3) {
         document.getElementById("wednesday").classList.add("current");
-        if (hm>= 390 && hm<= 450) {
+        if (hm>= 510 && hm<= 570) {
             document.getElementById("w1").classList.add("ctime");
-        } else if (hm >= 485 && hm <= 545) {
+        } else if (hm >= 595 && hm <= 655) {
             document.getElementById("w2").classList.add("ctime");
-        } else if (hm>= 565 && hm<= 625) {
+        } else if (hm>= 675 && hm<= 735) {
             document.getElementById("w3").classList.add("ctime");
-        } else if (hm>= 725 && hm<= 785) {
+        } else if (hm>= 755 && hm<= 815) {
             document.getElementById("w4").classList.add("ctime");
         } else {
             document.getElementById("sunday").innerHTML = ("It's break Time");
@@ -45,13 +45,13 @@ function random() {
 
     } else if (n == 4) {
         document.getElementById("thursday").classList.add("current");
-        if (hm>= 390 && hm<= 450) {
+        if (hm>= 510 && hm<= 570) {
             document.getElementById("th1").classList.add("ctime");
-        } else if (hm >= 485 && hm <= 545) {
+        } else if (hm >= 595 && hm <= 655) {
             document.getElementById("th2").classList.add("ctime");
-        } else if (hm>= 565 && hm<= 625) {
+        } else if (hm>= 675 && hm<= 735) {
             document.getElementById("th3").classList.add("ctime");
-        } else if (hm>= 725 && hm<= 785) {
+        } else if (hm>= 755 && hm<= 815) {
             document.getElementById("th4").classList.add("ctime");
         } else {
             document.getElementById("sunday").innerHTML = ("It's break Time");
@@ -61,13 +61,13 @@ function random() {
     } else if (n == 5) {
         document.getElementById("friday").classList.add("current");
 
-        if (hm>= 390 && hm<= 450) {
+        if (hm>= 510 && hm<= 570) {
             document.getElementById("f1").classList.add("ctime");
-        } else if (hm >= 485 && hm <= 545) {
+        } else if (hm >= 595 && hm <= 655) {
             document.getElementById("f2").classList.add("ctime");
-        } else if (hm>= 565 && hm<= 625) {
+        } else if (hm>= 675 && hm<= 735) {
             document.getElementById("f3").classList.add("ctime");
-        } else if (hm>= 725 && hm<= 785) {
+        } else if (hm>= 755 && hm<= 815) {
             document.getElementById("f4").classList.add("ctime");
         } else {
             document.getElementById("sunday").innerHTML = ("It's break Time");
@@ -77,13 +77,13 @@ function random() {
     } else if (n == 6) {
         document.getElementById("saturday").classList.add("current");
 
-        if (hm>= 390 && hm<= 450) {
+        if (hm>= 510 && hm<= 570) {
             document.getElementById("s1").classList.add("ctime");
-        } else if (hm >= 485 && hm <= 545) {
+        } else if (hm >= 595 && hm <= 655) {
             document.getElementById("s2").classList.add("ctime");
-        } else if (hm>= 565 && hm<= 625) {
+        } else if (hm>= 675 && hm<= 735) {
             document.getElementById("s3").classList.add("ctime");
-        } else if (hm>= 725 && hm<= 785) {
+        } else if (hm>= 755 && hm<= 815) {
             document.getElementById("s4").classList.add("ctime");
         } else {
             document.getElementById("sunday").innerHTML = ("It's break Time");
@@ -99,13 +99,13 @@ function random() {
     } else if (n == 1) {
         document.getElementById("monday").classList.add("current");
 
-        if (hm>= 390 && hm<= 450) {
+        if (hm>= 510 && hm<= 570) {
             document.getElementById("m1").classList.add("ctime");
-        } else if (hm >= 485 && hm <= 545) {
+        } else if (hm >= 595 && hm <= 655) {
             document.getElementById("m2").classList.add("ctime");
-        } else if (hm>= 565 && hm<= 625) {
+        } else if (hm>= 675 && hm<= 735) {
             document.getElementById("m3").classList.add("ctime");
-        } else if (hm>= 725 && hm<= 785) {
+        } else if (hm>= 755 && hm<= 815) {
             document.getElementById("m4").classList.add("ctime");
         } else {
             document.getElementById("sunday").innerHTML = ("It's break Time");
